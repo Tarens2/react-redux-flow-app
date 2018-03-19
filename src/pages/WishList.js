2924ed9480cc56e7341a188d97183401a2e0d1fa
Tarens2 <tarasAKK@mail.ru>
2018-03-20 02:05:37 +0300

    Updates

--- src/pages/WishList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Typography from 'material-ui/Typography'
-import CatalogContainer from '../containers/CatalogContainer'
-export default () => (
-  <div>
-    <Typography variant="headline" gutterBottom>Избранное</Typography>
-    <CatalogContainer wishList={true}/>
-  </div>
-)
